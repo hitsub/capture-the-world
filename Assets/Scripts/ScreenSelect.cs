19366a39a238477c0ba730999a214073d5ad592f
ひつぶ <hitsumabushis@gmail.com>
2017-01-07 13:19:57 +0900

    セレクト画面の実装（途中）

--- Assets/Scripts/ScreenSelect.cs
+++ Assets/Scripts/ScreenSelect.cs
@@ -23,6 +23,8 @@ public class ScreenSelect : MonoBehaviour {
 	[SerializeField] Text p2Stat;
 
 	int p1SelectPos = 0, p2SelectPos = 0;
+	bool p1SelectFlag = true, p2SelectFlag = true;
+
 	string[] className = { "Assalt", "Bomber", "Charger", "Recon" };
 	string[] classStat = { "Damage", "FireRate", "RunSpeed", "Energy", "Recovery" };
 	string[] classFunc = { "Power", "FireRate", "Speed", "Energy", "Recovery" };
@@ -30,37 +32,73 @@ public class ScreenSelect : MonoBehaviour {
 
 	JsonNode json;
 
+	//Animation Const.
+	const string animGuageEase = "easeOutCubic";
+	const float animGuageTime = 0.3f;
+	const string animPositionEase = "easeOutBack";
+	const float animPositionTime = 0.3f;
 
 	// Use this for initialization
 	void Start () {
 		//TextAsset text = Resources.Load ("class") as TextAsset;
 		json = JsonNode.Parse (Resources.Load <TextAsset>("class").text);
 		for (int i = 0; i < classStat.Length; i++) {
-			print (i + ":" + (float)json ["Assalt"] [classStat [i]].Get<long> () / classMax [i]+"\n"+json ["Assalt"] [classStat [i]].Get<long> ()+" / "+classMax [i]);
-			iTween.ValueTo (gameObject, iTween.Hash ("from", 0f, "to", (float)json ["Assalt"] [classStat [i]].Get<long>() / classMax [i], "time",0.1f, "onupdate","SetP1Guage"+classFunc[i]));
+			//print (i + ":" + (float)json ["Assalt"] [classStat [i]].Get<long> () / classMax [i]+"\n"+json ["Assalt"] [classStat [i]].Get<long> ()+" / "+classMax [i]);
+			iTween.ValueTo (gameObject, iTween.Hash ("from", 0f, "to", (float)json ["Assalt"] [classStat [i]].Get<long> () / classMax [i], "time", 1f, "easetype", animGuageEase, "onupdate", "SetP1Guage" + classFunc [i]));
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//カメラの回転
 		transform.Rotate (0, -5f * Time.deltaTime, 0);
+
+		//キー入力
 		if (Input.GetKeyDown (KeyCode.UpArrow) && (p1SelectPos != 0)) {
 			p1SelectPos--;
-			iTween.ValueTo (gameObject, iTween.Hash ("from",100f-50f*(p1SelectPos+1),"to",100f-50f*(p1SelectPos),"time",0.1f,"onupdate","SetP1SelectPos"));
+			iTween.ValueTo (gameObject, iTween.Hash (
+				"from", 100f - 50f * (p1SelectPos + 1), 
+				"to", 100f - 50f * (p1SelectPos), 
+				"time", animPositionTime, 
+				"easetype", animPositionEase, 
+				"onupdate", "SetP1SelectPos"
+			));
 			for (int i = 0; i < classStat.Length; i++) {
-				iTween.ValueTo (gameObject, iTween.Hash ("from", (float)json [className[p1SelectPos+1]] [classStat [i]].Get<long>() / classMax [i], "to", (float)json [className[p1SelectPos]] [classStat [i]].Get<long>() / classMax [i], "time",0.1f, "onupdate","SetP1Guage"+classFunc[i]));
+				iTween.ValueTo (gameObject, iTween.Hash (
+					"from", (float)json [className [p1SelectPos + 1]] [classStat [i]].Get<long> () / classMax [i], 
+					"to", (float)json [className [p1SelectPos]] [classStat [i]].Get<long> () / classMax [i], 
+					"time", animGuageTime, 
+					"easetype", animGuageEase, 
+					"onupdate", "SetP1Guage" + classFunc [i]
+				));
 			}
 		}
 		if (Input.GetKeyDown (KeyCode.DownArrow) && (p1SelectPos != 3)) {
 			p1SelectPos++;
-			iTween.ValueTo (gameObject, iTween.Hash ("from",100f-50f*(p1SelectPos-1),"to",100f-50f*(p1SelectPos),"time",0.1f,"onupdate","SetP1SelectPos"));
+			iTween.ValueTo (gameObject, iTween.Hash (
+				"from", 100f - 50f * (p1SelectPos - 1), 
+				"to", 100f - 50f * (p1SelectPos), 
+				"time", animPositionTime, 
+				"easetype", animPositionEase, 
+				"onupdate", "SetP1SelectPos"
+			));
 			for (int i = 0; i < classStat.Length; i++) {
-				//print (i + " from:" + (float)json [className [p1SelectPos - 1]] [classStat [i]].Get<long> () / classMax [i]+" to:"+(float)json [className[p1SelectPos]] [classStat [i]].Get<long>() / classMax [i]);
-				iTween.ValueTo (gameObject, iTween.Hash ("from", (float)json [className[p1SelectPos-1]] [classStat [i]].Get<long>() / classMax [i], "to", (float)json [className[p1SelectPos]] [classStat [i]].Get<long>() / classMax [i], "time",0.1f, "onupdate","SetP1Guage"+classFunc[i]));
+				iTween.ValueTo (gameObject, iTween.Hash (
+					"from", (float)json [className [p1SelectPos - 1]] [classStat [i]].Get<long> () / classMax [i], 
+					"to", (float)json [className [p1SelectPos]] [classStat [i]].Get<long> () / classMax [i], 
+					"time", animGuageTime, 
+					"easetype", animGuageEase, 
+					"onupdate", "SetP1Guage" + classFunc [i]
+				));
 			}
 		}
-	}
+		if (Input.GetKeyDown (KeyCode.Return) && p1SelectFlag) {
+			p1SelectFlag = false;
+			p1Stat.text = "Waiting";
 
+		}
+	}
+	//P1
 	void SetP1SelectPos (float y){
 		p1Select.anchoredPosition = new Vector2 (-130, y);
 	}
@@ -78,5 +116,24 @@ public class ScreenSelect : MonoBehaviour {
 	}
 	void SetP1GuageRecovery (float value){
 		p1GuageRecovery.fillAmount = value;
+	}
+	//P2
+	void SetP2SelectPos (float y){
+		p2Select.anchoredPosition = new Vector2 (130, y);
+	}
+	void SetP2GuagePower (float value){
+		p2GuagePower.fillAmount = value;
+	}
+	void SetP2GuageFireRate (float value){
+		p2GuageFirerate.fillAmount = value;
+	}
+	void SetP2GuageSpeed (float value){
+		p2GuageSpeed.fillAmount = value;
+	}
+	void SetP2GuageEnergy (float value){
+		p2GuageEnergy.fillAmount = value;
+	}
+	void SetP2GuageRecovery (float value){
+		p2GuageRecovery.fillAmount = value;
 	}
 }
